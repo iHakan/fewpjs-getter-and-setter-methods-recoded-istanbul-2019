b210a14015a852336f4080ef0c15085fe1793e5a
Hakan Yildiz <hakan@re-coded.com>
2020-04-11 20:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius=radius
+  }
+  
+  get diameter(){
+    return this.radius*2;
+  }
+  
+  get circumference(){
+    return Math.PI*(this.radius);
+  }
+  get area(){
+    
+  }
+}
